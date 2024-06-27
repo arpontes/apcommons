ef8c540aeed7aa412f1c907d423fdc6a64067454
Andre Pontes <andrepontes@cvccorp.com.br>
2024-06-27 02:27:41 -0300

    new version

--- src/logging.ts
+++ src/logging.ts
@@ -8,20 +8,18 @@ function getDiffMs(fromDt: Date) {
 }
 
 export type Logger = <T>(result: string, complData?: T, exception?: any) => { action: string; result?: string; time: number; error?: true; errorData?: string } & T;
-export function createLogger(action: string, fixedComplData?: any, fnLogAction: <T>(logObject: T) => T: Logger {
+export function createLogger(action: string, fixedComplData?: any, fnLogAction?: <T>(logObject: T) => T): Logger {
     const dt = new Date();
-    const action = fnLogAction ? fnLogAction : defaultLog;
+    const fnInternalAction = fnLogAction ? fnLogAction : defaultLog;
     return (result: string, complData?: any, exception?: any) =>
-        action(
-            {
-                action,
-                ...(fixedComplData ?? {}),
-                result: result == null || result === "" ? undefined : result,
-                time: getDiffMs(dt),
-                ...(complData ?? {}),
-                ...(exception ? { error: true, errorData: JSON.stringify(exception, Object.getOwnPropertyNames(exception)) } : {})
-            }
-        );
+        fnInternalAction({
+            action,
+            ...(fixedComplData ?? {}),
+            result: result == null || result === "" ? undefined : result,
+            time: getDiffMs(dt),
+            ...(complData ?? {}),
+            ...(exception ? { error: true, errorData: JSON.stringify(exception, Object.getOwnPropertyNames(exception)) } : {})
+        });
 }
 export function createLoggerOnLogger(origin: Logger, newFixedComplData: any): Logger {
     return (result: string, complData?: any, exception?: any) => origin(result, { ...(newFixedComplData ?? {}), ...(complData ?? {}) }, exception);
