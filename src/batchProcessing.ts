32f313b83092e35f46f816ba6bb0f3baa8297c9c
Andre Pontes <andrepontes@cvccorp.com.br>
2024-07-18 06:03:45 -0300

    adding callback for block

--- src/batchProcessing.ts
+++ src/batchProcessing.ts
@@ -1,51 +1,57 @@
 import { Logger } from "./logging";
 
-type PossibleReturn = {
+export type ReturnSuccess = {
     stop?: boolean;
-    error?: boolean;
+    error?: false;
 };
-export async function processBatch<T, K extends PossibleReturn, Y>(
-    readData: Array<T>, 
-    batchSize: number, 
-    logger: Logger, 
-    fnProcessData: (item: T, logger: Logger) => Promise<K>, 
-    fnProcessBlock?: (successItems: Array<Y>, errorItems: Array<K>, logger: Logger) => Promise
+export type ReturnError = {
+    stop?: boolean;
+    error: true;
+};
+export async function processBatch<T, K extends ReturnSuccess, E extends ReturnError>(
+    readData: Array<T>,
+    batchSize: number,
+    logger: Logger,
+    fnProcessData: (item: T, logger: Logger) => Promise<K | E>,
+    fnProcessBlock?: (totalProcesses: number, successItems: Array<K>, errorItems: Array<E>) => Promise<void>
 ) {
     const mustProcessBlocks = fnProcessBlock != null;
-    const processData = { threads: [] as Array<Promise<K>>, totalProcessed: 0, totalErrors: 0, successes: [] as Y[], errors: [] as K[] };
+    const processData = { threads: [] as Array<Promise<K | E>>, totalProcessed: 0, totalErrors: 0, errors: [] as E[] };
     for await (const obj of readData) {
         processData.threads.push(fnProcessData(obj, logger));
         if (processData.threads.length === batchSize) {
-            const shouldContinue = await processBlock(processData, logger);
+            const shouldContinue = await processBlock();
             if (!shouldContinue) break;
         }
     }
-    if (processData.threads.length > 0) await processBlock(processData, logger);
+    if (processData.threads.length > 0) await processBlock();
     return { totalProcessed: processData.totalProcessed, totalErrors: processData.totalErrors, errors: processData.errors };
 
     async function processBlock() {
         const msgs = await Promise.all(processData.threads);
-    
+
         processData.threads = [];
+        processData.totalProcessed += msgs.length;
+
         let shouldContinue = true;
+        const blockResult = { successes: [] as K[], errors: [] as E[] };
         for (var j = 0; j < msgs.length; j++) {
-            processData.totalProcessed++;
             const retData = msgs[j];
-            if (retData?.stop) shouldContinue = false;
-            if (retData?.error === true) {
+
+            const hasError = retData?.error === true;
+            if (hasError) {
                 processData.totalErrors++;
                 processData.errors.push(retData);
-            } else if (mustProcessBlocks) {
-                processData.successes.push(retData);
             }
-            logger("", retData);
+
+            if (mustProcessBlocks) {
+                if (hasError) blockResult.errors.push(retData);
+                else blockResult.successes.push(retData);
+            }
+
+            if (retData?.stop) shouldContinue = false;
         }
-        if (mustProcessBlocks){
-            await fnProcessBlock(processData.successes, processData.errors, logger);
-            processData.totalProcessed = processData.totalErrors = 0;
-            processData.successes = [];
-            processData.errors = [];
-        }
+        if (mustProcessBlocks) await fnProcessBlock(processData.totalProcessed, blockResult.successes, blockResult.errors);
         return shouldContinue;
     }
 }
